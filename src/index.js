e049f68e22918a222df779a9ab488154511f3859
joselatines <joselatinestranslator@gmail.com>
2024-11-30 00:37:54 -0400

    feat: added starting info

--- src/index.js
+++ src/index.js
@@ -4,3 +4,68 @@ import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+
+const projects_session_1 = [
+  {
+    title: "Infografía sobre la gestión de proyectos",
+    description: `Infografia acerca la gestión de
+proyectos, la cual refleja la
+importancia de la misma en
+todo tipo de proyectos, bien
+sea de software o de otra
+índole. En la infografia
+compartimos un definición
+propia, se explicaron sus
+características, importancia e
+implementación.`,
+    url: "https://drive.google.com/file/d/188dAqsLBwfYtntBwTcysnLiqxdhSvtks/view",
+    image: "/public/infografia-proyectos.png",
+  },
+
+  {
+    title: "Co-evaluacion infografia",
+    description: `Evaluación de pares o también llamada co-evaluación (en el bajo mundo), la cual se analizó la infografia del grupo #12 el cual realizo una infografia acerca la gestión de proyectos. Dicha co-evaluacion sirvió para que el equipo #12 pudiera hacerle mejora a su infografia y entregar un mejor trabajo.`,
+    url: "https://drive.google.com/file/d/1CKOYgB5fgxbUo-LUmMLy_tfsoIjqGkYf/view",
+    image: "/public/coevaluacion.png",
+  },
+  {
+    title: " ¿Por qué es importante para el levantamiento de información? ",
+    description: `agregar una`,
+    url: "",
+    image: "/public/levantamiento-informacion.png",
+
+  },
+  {
+    title: "Tu receta tecnológica: ¡Qué no se te escape ningún bug!",
+    description: `Organizador grafico acerca el diagnostico pariticipativo  es un proceso colaborativo donde la comunidad identifica y analiza problemas y necesidades, asegurando soluciones relevantes y efectivas basadas en la participación activa de sus miembros. `,
+    url: "https://drive.google.com/file/d/1AsInjAHbbizLaMWwm2hbFjYsyNGUJrMc/view",
+    image: "/public/tureceta.png",
+
+  },
+];
+
+const projects_session_2 = [
+  {
+    title: "Informe del DVP",
+    description: `Dpcument paa pryecto caso de estudio`,
+    url: "https://docs.google.com/document/d/1rDZXIVYBBCmOJyQQoAkQwrZcee4K4sm7P8KIu86MubQ/edit?usp=sharing",
+    image: "/public/dvp.png",
+  },
+
+  {
+    title: "Informe del Scrum actividad: Taller #1",
+    description: `Agregar`,
+    url: "https://docs.google.com/document/d/1fmEoT9Nx-WjPQpzpy_BQw3dog3nGCNGXeXDOxLL_w9Q/edit?tab=t.0#heading=h.gjdgxs",
+    image: "/public/taller1.png",
+  },
+  {
+    title: "INFORME DINÁMICA SIMULACIÓN DE UN SPRINT",
+    description: `agregar una`,
+    url: "https://docs.google.com/document/d/1zHp7nerKxuiFyxd2VTCvztolEUUlaAKO/edit#heading=h.gjdgxs",
+    image: "/public/simulacion-sprint.png",
+
+  },
+
+];
+
+
